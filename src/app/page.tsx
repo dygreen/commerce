b93c2e29c5b0e705f83ea38cffba808ebaefef3f
dygreen <ehdus9877@naver.com>
2024-08-05 21:21:43 +0900

    feat : tailwind, emotion 추가 및 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
+import { css } from '@node_modules/@emotion/react'
 
 export default function Home() {
     const [products, setProducts] = useState<
@@ -57,8 +58,22 @@ export default function Home() {
     return (
         <main>
             <div>
-                <input ref={inputRef} type="text" placeholder="name" />
-                <button onClick={handleClick}>Add Jacket</button>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    placeholder="name"
+                    className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                />
+                <button
+                    css={css`
+                        background-color: hotpink;
+                        padding: 16px;
+                        border-radius: 8px;
+                    `}
+                    onClick={handleClick}
+                >
+                    Add Jacket
+                </button>
             </div>
             <div>
                 <p>Product List</p>
